881da8b041089fee1456317cb7c4da7d3f621f8d
Washington da Costa Silva <washington3636@gmail.com>
2021-06-13 16:02:24 -0300

    Testes

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -35,5 +35,27 @@ class UserTest extends TestCase
                 'message' => "Successfully created user!",
             ]);
     }
+
+    public function testUserLogin()
+    {
+        $name = $this->faker->name();
+       	$email = $this->faker->email();
+
+        $user = new User([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($this->password)
+        ]);        
+        
+        $user->save();     
+        
+        $response = $this->postJson('/api/auth/login', [
+            'email' => $email,
+            'password' => $this->password
+        ]);
+            
+        $response->assertStatus(200);
+        $this->assertAuthenticated();
+    }//testUserLogin
     
 }
