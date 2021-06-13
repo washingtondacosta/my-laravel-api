28ac1b168125790c8b85d339794939f439cf2da7
Washington da Costa Silva <washington3636@gmail.com>
2021-06-13 15:57:24 -0300

    Testes

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -10,44 +10,30 @@ use App\User;
 
 class UserTest extends TestCase
 {
-	
-	use WithFaker;
+   
+    use WithFaker;
 
-	private $password = "mypassword";
-	
-	public function testUserCreation()
-	{
-		
-		$name = $this->faker->name();
-		$email = $this->faker->email();
+    private $password = "mypassword";
+    
+    public function testUserCreation()
+    {
+       	
+       	$name = $this->faker->name();
+       	$email = $this->faker->email();
 
-		$response = $this->postJson('/api/auth/signup', [
-			'name' => $name, 
-			'email' => $email,
-			'password' => $this->password, 
-			'password_confirmation' => $this->password
-		]); 
+        $response = $this->postJson('/api/auth/signup', [
+            'name' => $name, 
+            'email' => $email,
+            'password' => $this->password, 
+            'password_confirmation' => $this->password
+        ]); 
 
 
-		$response
-		->assertStatus(201)
-		->assertExactJson([
-			'message' => "Successfully created user!",
-		]);
-	}
-
-	public function test_login(){
-
-        $user = User::factory()->create();
-        $response = $this->from('/login')->post('/login', [
-            'email' => $user->email,
-            'password' => $user->password
-        ]);
-        $response->assertRedirect('/login');
-        $response->assertSessionHasErrors('email');
-        $this->assertTrue(session()->hasOldInput('email'));
-        $this->assertFalse(session()->hasOldInput('password'));
-        $this->assertGuest();
+        $response
+            ->assertStatus(201)
+            ->assertExactJson([
+                'message' => "Successfully created user!",
+            ]);
     }
-	
+    
 }
