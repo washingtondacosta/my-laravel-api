9154524f9c812aeff7d8bd98c2cbde5b23e54f76
Washington da Costa Silva <washington3636@gmail.com>
2021-06-13 15:47:05 -0300

    New Teste

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -10,30 +10,53 @@ use App\User;
 
 class UserTest extends TestCase
 {
-   
-    use WithFaker;
+	
+	use WithFaker;
 
-    private $password = "mypassword";
-    
-    public function testUserCreation()
-    {
-       	
-       	$name = $this->faker->name();
-       	$email = $this->faker->email();
+	private $password = "mypassword";
+	
+	public function testUserCreation()
+	{
+		
+		$name = $this->faker->name();
+		$email = $this->faker->email();
 
-        $response = $this->postJson('/api/auth/signup', [
-            'name' => $name, 
-            'email' => $email,
-            'password' => $this->password, 
-            'password_confirmation' => $this->password
-        ]); 
+		$response = $this->postJson('/api/auth/signup', [
+			'name' => $name, 
+			'email' => $email,
+			'password' => $this->password, 
+			'password_confirmation' => $this->password
+		]); 
 
 
-        $response
-            ->assertStatus(201)
-            ->assertExactJson([
-                'message' => "Successfully created user!",
-            ]);
-    }//testUserCreation
-    
+		$response
+		->assertStatus(201)
+		->assertExactJson([
+			'message' => "Successfully created user!",
+		]);
+	}
+
+	public function testUserLogin()
+	{
+		$name = $this->faker->name();
+		$email = $this->faker->email();
+
+		$user = new User([
+			'name' => $name,
+			'email' => $email,
+			'password' => bcrypt($this->password)
+		]);        
+		
+		$user->save();     
+		
+		$response = $this->postJson('/api/auth/login', [
+			'email' => $email,
+			'password' => $this->password
+		]);
+
+		
+		$response->assertStatus(200);
+		$this->assertAuthenticated();
+	}
+	
 }
